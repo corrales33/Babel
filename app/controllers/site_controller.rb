9e5978409051c1be325892189153179f45f5cb8d
Alberto Diez <albertodiezcalvo@hotmail.com>
2015-06-03 15:19:11 +0200

    View show user created

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,9 @@ class SiteController < ApplicationController
 	def welcome
 		@users = User.last_users_registered(4)
 		@places = Place.last_places_registered(4)
+		@meetings = Meeting.last_meetings_done(5)
+		#@user = User.find params[:user_id]
+		#@meeting = @user.meetings
 	end
 
 end
--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,7 @@
 class Meeting < ActiveRecord::Base
 	belongs_to :user
+
+	def self.last_meetings_done param
+		meetings = Meeting.order(created_at: :desc).limit(param)
+	end
 end
